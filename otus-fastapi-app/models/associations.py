bedf76cd71cd4c10ffde827843c2abee22558281
Elvira Ondar <elvira_kharunova@mail.ru>
2025-08-22 15:57:53 +0300

    Удален otus-fastapi-app из репозитория

--- otus-fastapi-app/models/associations.py
+++
@@ -1,9 +0,0 @@
-from sqlalchemy import Table, Column, Integer, ForeignKey
-from db.session import Base
-
-post_tags = Table(
-    'post_tags',
-    Base.metadata,
-    Column('post_id', Integer, ForeignKey('posts.id'), primary_key=True),
-    Column('tag_id', Integer, ForeignKey('tags.id'), primary_key=True)
-)
